7c28ff4cfe19d9bf738f86dd924b9436ec27ebd4
Yingqing Shan <yingqing.shan@sayweee.com>
2022-08-01 16:56:40 +0800

    fix 名称

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@
 #
 # Copyright 2013 - 2018 Giovanni Cannata
 #
-# This file is part of ldap3.
-# ldap3 is free software: you can redistribute it and/or modify
+# This file is part of google_ldap3.
+# google_ldap3 is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ldap3 is distributed in the hope that it will be useful,
+# google_ldap3 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ldap3 in the COPYING and COPYING.LESSER files.
+# along with google_ldap3 in the COPYING and COPYING.LESSER files.
 # If not, see <http://www.gnu.org/licenses/>.
 
 import os
@@ -30,32 +30,42 @@ from distutils.command.clean import clean
 from distutils import log
 from setuptools import setup
 
-version_dict = load(open('_version.json', 'r'))
-version = str(version_dict['version'])
-author = str(version_dict['author'])
-email = str(version_dict['email'])
-license = str(version_dict['license'])
-url = str(version_dict['url'])
-description = str(version_dict['description'])
-package_name = str(version_dict['package_name'])
-package_folder = str(version_dict['package_folder'])
-status = str(version_dict['status'])
+# {
+#     "package_folder": ".",
+#     "email": "yshan20@su.edu",
+#     "package_name": "google-google_ldap3",
+#     "url": "https://github.com/462548187/google-ldap3.git",
+#     "description": "A strictly RFC 4510 conforming LDAP V3 pure Python client library",
+#     "author": "Leo",
+#     "version": "0.0.5",
+#     "license": "LGPL v3"
+# }
+
+version = "0.0.10"
+author = "Leo"
+email = "yshan20@su.edu"
+license = "LGPL v3"
+url = "https://github.com/462548187/google-ldap3.git"
+description = ""
+package_name = "google_ldap3"
+package_folder = "."
 
 long_description = str(open('README.rst').read())
-packages = ['ldap3',
-            'ldap3.abstract',
-            'ldap3.core',
-            'ldap3.operation',
-            'ldap3.protocol',
-            'ldap3.protocol.sasl',
-            'ldap3.protocol.schemas',
-            'ldap3.protocol.formatters',
-            'ldap3.strategy',
-            'ldap3.utils',
-            'ldap3.extend',
-            'ldap3.extend.novell',
-            'ldap3.extend.microsoft',
-            'ldap3.extend.standard']
+packages = ['google_ldap3',
+            'google_ldap3.abstract',
+            'google_ldap3.core',
+            'google_ldap3.operation',
+            'google_ldap3.protocol',
+            'google_ldap3.protocol.sasl',
+            'google_ldap3.protocol.schemas',
+            'google_ldap3.protocol.formatters',
+            'google_ldap3.strategy',
+            'google_ldap3.utils',
+            'google_ldap3.extend',
+            'google_ldap3.extend.novell',
+            'google_ldap3.extend.microsoft',
+            'google_ldap3.extend.standard',
+            ]
 
 setup_kwargs = {'packages': packages,
                 'package_dir': {'': package_folder}}
@@ -65,7 +75,7 @@ class Clean(clean):
     def run(self):
         clean.run(self)
         # Let's clean compiled *.py[c,o] *.c *.so
-        for subdir in ('ldap3',):
+        for subdir in ('google_ldap3',):
             root = os.path.join(os.path.dirname(__file__), subdir)
             for dirname, dirs, _ in os.walk(root):
                 for to_remove_filename in glob.glob('{0}/*.py[ocx]'.format(dirname)):
@@ -83,7 +93,7 @@ setup_kwargs['cmdclass'] = {'clean': Clean}
 
 setup(name=package_name,
       version=version,
-      install_requires=[i.strip() for i in open('requirements.txt').readlines()],
+      install_requires=['pyasn1>=0.4.8'],
       license=license,
       author=author,
       author_email=email,
@@ -91,8 +101,7 @@ setup(name=package_name,
       long_description=long_description,
       keywords='python3 python2 ldap',
       url=url,
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Developers',
+      classifiers=['Intended Audience :: Developers',
                    'Intended Audience :: System Administrators',
                    'Operating System :: MacOS :: MacOS X',
                    'Operating System :: Microsoft :: Windows',
